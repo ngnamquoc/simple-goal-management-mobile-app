91fa4dac8752947a117bc3804c50623382a58a34
ngnamquoc <nguyennamquoc.mar@gmail.com>
2024-07-21 08:31:31 -0400

    improve layout

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Another Text</Text>
-      <Text>Hello World!</Text>
-      <Button title="TAP ME!"></Button>
-
+    <View style={myStyles.appContainer}>
+      <View style={myStyles.inputContainer}>
+        <TextInput
+          style={myStyles.textInput}
+          placeholder="Enter your course goal..."
+        />
+        <Button title="Add goal" />
+      </View>
+      <View style={myStyles.goalsContainer}>
+        <Text >List of goals</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+//initialize an instance from StyleSheet
+const myStyles = StyleSheet.create({
+  appContainer: {
+    flex:1,
+    padding: 50,
+    paddingHorizontal:16
   },
+  inputContainer: {
+    flexDirection: "row", //by default is "column"
+    flex:1,
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor:"#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight:8,
+    padding:8
+
+  },
+  goalsContainer:{
+    flex:5,
+  }
 });
