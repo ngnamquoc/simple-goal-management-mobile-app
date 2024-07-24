2c1a4d1dfd1e3b461eafc6776ec7cd7a94c031b5
ngnamquoc <nguyennamquoc.mar@gmail.com>
2024-07-24 13:06:45 -0400

    add android ripple effect and ios styling to deleted pressed component

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,8 +1,12 @@
 import { StyleSheet, View, Text, Pressable } from "react-native";
 
-export default function GoalItem({ text,deleteGoalHandler,id }) {
+export default function GoalItem({ text, deleteGoalHandler, id }) {
   return (
-    <Pressable onPress={deleteGoalHandler.bind(this,id)}>
+    <Pressable
+      android_ripple={{ color: "#dddddd" }}
+      onPress={deleteGoalHandler.bind(this, id)}
+      style={({pressed})=>pressed && myStyles.pressedItem}
+    >
       <View style={myStyles.goalItem}>
         <Text style={myStyles.goalText}>{text}</Text>
       </View>
@@ -14,10 +18,13 @@ const myStyles = StyleSheet.create({
   goalItem: {
     margin: 8,
     borderRadius: 6,
-    padding: 6,
     backgroundColor: "#5e0acc",
   },
   goalText: {
     color: "white",
+    padding: 6,
   },
+  pressedItem:{
+    opacity:0.5
+  }
 });
